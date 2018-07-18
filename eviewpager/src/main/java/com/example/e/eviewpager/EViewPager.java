ff612f07af1581d5acd76400958d74d45b215231
chicunxiang <v_chicunxiang@baidu.com>
2018-07-18 14:45:39 +0800

    update

---
+++ eviewpager/src/main/java/com/example/e/eviewpager/EViewPager.java
@@ -0,0 +1,20 @@
+package com.example.e.eviewpager;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * Created by v_chicunxiang on 2018/7/18.
+ */
+
+public class EViewPager extends ViewPager {
+
+    public EViewPager(Context context) {
+        this(context, null);
+    }
+
+    public EViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ sample/src/main/java/com/example/e/sample/SampleViewPagerActivity.java
@@ -0,0 +1,13 @@
+package com.example.e.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_view_pager);
+    }
+}
