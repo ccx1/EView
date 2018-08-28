a5210207d24cfc3165012fe4da85c389c1e22db9
ccx <ci250454344@163.com>
2018-08-28 15:34:59 +0800

    update

--- sample/src/main/java/com/example/e/sample/VideoActivity.java
+++ sample/src/main/java/com/example/e/sample/VideoActivity.java
@@ -19,46 +19,6 @@ public class VideoActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         EVideoView viewById = (EVideoView) findViewById(R.id.video);
         viewById.setVideoPath("rtmp://live.hkstv.hk.lxdns.com/live/hks");
-        viewById.setListener(new VideoPlayerListener() {
-            @Override
-            public void onVideoSizeChanged(IMediaPlayer iMediaPlayer, int i, int i1, int i2, int i3) {
 
-            }
-
-            @Override
-            public void onTimedText(IMediaPlayer iMediaPlayer, IjkTimedText ijkTimedText) {
-
-            }
-
-            @Override
-            public void onSeekComplete(IMediaPlayer iMediaPlayer) {
-
-            }
-
-            @Override
-            public void onPrepared(IMediaPlayer iMediaPlayer) {
-
-            }
-
-            @Override
-            public boolean onInfo(IMediaPlayer iMediaPlayer, int i, int i1) {
-                return false;
-            }
-
-            @Override
-            public boolean onError(IMediaPlayer iMediaPlayer, int i, int i1) {
-                return false;
-            }
-
-            @Override
-            public void onCompletion(IMediaPlayer iMediaPlayer) {
-
-            }
-
-            @Override
-            public void onBufferingUpdate(IMediaPlayer iMediaPlayer, int i) {
-
-            }
-        });
     }
 }
