cbd6a6547fd77522970371ec6507464144e6fa9e
ccx <ci250454344@163.com>
2018-09-03 16:11:36 +0800

    添加了一些花头劲

--- ezxing/src/main/java/com/ccx/ezxing/view/ViewfinderView.java
+++ ezxing/src/main/java/com/ccx/ezxing/view/ViewfinderView.java
@@ -6,8 +6,11 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -20,12 +23,9 @@ import java.util.List;
 
 public final class ViewfinderView extends View {
 
-    private static final int[] SCANNER_ALPHA         = {0, 64, 128, 192, 255, 192, 128, 64};
-    private static final long  ANIMATION_DELAY       = 80L;
-    private static final int   CURRENT_POINT_OPACITY = 0xA0;
-    private static final int   MAX_RESULT_POINTS     = 20;
-    private static final int   POINT_SIZE            = 6;
-    private final Context mContext;
+    private static final long ANIMATION_DELAY   = 20L;
+    private static final int  MAX_RESULT_POINTS = 20;
+    private static final int  POINT_SIZE        = 6;
 
     private       CameraManager     cameraManager;
     private final Paint             paint;
@@ -33,20 +33,21 @@ public final class ViewfinderView extends View {
     private final int               maskColor;
     private final int               resultColor;
     //    private final int               laserColor;
-    private       int               scannerAlpha;
     private       List<ResultPoint> possibleResultPoints;
+    private       boolean           isDraw;
+    private int mBottom = -1;
+    private int mTop    = -1;
+    private Shader mShader;
 
     // This constructor is used when the class is built from an XML resource.
     public ViewfinderView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.mContext = context;
         // Initialize these once for performance rather than calling them every time in onDraw().
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         Resources resources = getResources();
         maskColor = resources.getColor(R.color.viewfinder_mask);
         resultColor = resources.getColor(R.color.result_view);
 //        laserColor = resources.getColor(R.color.viewfinder_laser);
-        scannerAlpha = 0;
         possibleResultPoints = new ArrayList<>(5);
     }
 
@@ -76,30 +77,76 @@ public final class ViewfinderView extends View {
         canvas.drawRect(frame.right + 1, frame.top, width, frame.bottom + 1, paint);
         canvas.drawRect(0, frame.bottom + 1, width, height, paint);
 
+        paint.setColor(Color.WHITE);
+
+        paint.setStrokeWidth(10f);
+        Path path = new Path();
+        paint.setStyle(Paint.Style.STROKE);
+        int left   = frame.left;
+        int top    = frame.top;
+        int right  = frame.right;
+        int bottom = frame.bottom;
+        // 出来是高度
+        int RectHeight = (bottom - top) / 3;
+        int RectWidth  = (right - left) / 3;
+
+        // 因为是正方形，用哪个都可以
+
+        path.moveTo(left, bottom - RectHeight);
+        path.lineTo(left, bottom);
+        path.lineTo(left + RectHeight, bottom);
 
 
+        // left + RectHeight + 150
+        path.rMoveTo(RectWidth, 0);
 
-        if (resultBitmap != null) {
-            // Draw the opaque result bitmap over the scanning rectangle
-            paint.setAlpha(CURRENT_POINT_OPACITY);
-            canvas.drawBitmap(resultBitmap, null, frame, paint);
-        } else {
-            // Draw a red "laser scanner" line through the middle to show decoding is active
-            paint.setColor(Color.parseColor("#26A69A"));
-            paint.setAlpha(SCANNER_ALPHA[scannerAlpha]);
-            scannerAlpha = (scannerAlpha + 1) % SCANNER_ALPHA.length;
-            int middle = frame.height() / 2 + frame.top;
-            canvas.drawRect(frame.left + 2, middle - 1, frame.right - 1, middle + 2, paint);
+        path.lineTo(right, bottom);
+        path.lineTo(right, bottom - RectHeight);
 
 
-            // Request another update at the animation interval, but only repaint the laser line,
-            // not the entire viewfinder mask.
-            postInvalidateDelayed(ANIMATION_DELAY,
-                    frame.left - POINT_SIZE,
-                    frame.top - POINT_SIZE,
-                    frame.right + POINT_SIZE,
-                    frame.bottom + POINT_SIZE);
+        path.rMoveTo(0, -RectHeight);
+        path.lineTo(right, top);
+        path.lineTo(right - RectWidth, top);
+
+        // 偏移之后，450，300
+        path.rMoveTo(-RectWidth, 0);
+        path.lineTo(left, top);
+        path.lineTo(left, top + RectHeight);
+
+        canvas.drawPath(path, paint);
+
+        if (mTop > frame.bottom - 100) {
+            isDraw = true;
+        } else if (mTop < frame.top + 100) {
+            isDraw = false;
         }
+
+        if (mTop == -1 || mBottom == -1) {
+            mTop = top + 5;
+            mBottom = mTop + 5;
+        }
+
+        if (isDraw) {
+            mTop -= 10;
+            mBottom -= 10;
+        } else {
+            mTop += 10;
+            mBottom += 10;
+        }
+
+        // 画中间的动画线
+        paint.setStyle(Paint.Style.FILL);
+        mShader = new LinearGradient(frame.left, mTop, frame.right, mBottom, new int[]{Color.TRANSPARENT, Color.parseColor("#aaffffff"), Color.parseColor("#5526A69A"), Color.parseColor("#aaffffff"), Color.TRANSPARENT}, new float[]{0.1f, 0.2f, 0.5f, 0.8f, 1f}, LinearGradient.TileMode.CLAMP);
+        paint.setShader(mShader);
+//        paint.setAlpha(100);
+        canvas.drawRect(left, mTop, frame.right, mBottom, paint);
+        paint.setShader(null);
+        postInvalidateDelayed(ANIMATION_DELAY,
+                frame.left - POINT_SIZE,
+                frame.top - POINT_SIZE,
+                frame.right + POINT_SIZE,
+                frame.bottom + POINT_SIZE);
+
 
     }
 
